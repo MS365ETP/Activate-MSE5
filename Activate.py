00617b0712c0d4b7bbf5da590fa5bb83b7dda0b3
MS365ETP <135505977+MS365ETP@users.noreply.github.com>
2023-07-03 18:26:36 +0800

    Create Activate.py
    This file will help you to activate Microsoft 365 E5 Developer subscriptions. If you want to set up this subscription, you need a Microsoft Account and a telephone that can send the email, and you must go to https://developer.microsoft.com/en-us/microsoft-365/profile.

---
+++ Activate.py
@@ -0,0 +1,24 @@
+import time
+
+def countdown(minutes):
+    seconds = minutes * 60
+    while seconds > 0:
+        mins, secs = divmod(seconds, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        seconds -= 1
+    print("Time's up!")
+
+def main():
+    print("欢迎使用专注时钟！")
+    while True:
+        try:
+            minutes = int(input("请输入专注时长（分钟）："))
+            break
+        except ValueError:
+            print("无效的输入，请重新输入一个整数值。")
+    countdown(minutes)
+
+if __name__ == '__main__':
+    main()
